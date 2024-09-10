fb0fad85879a8841487ffc3e00e5094412614e8b
Grigoriy Pridatkn <pridatkin@gmail.com>
2024-09-10 11:43:52 +0300

    Add a page along the news/ url

--- News/urls.py
+++ News/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from News.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-	path('news/', include('News.urls')),
+	path('', index),
 ]
