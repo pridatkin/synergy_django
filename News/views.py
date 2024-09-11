dcbb5628599d9b2e5d54b7b118aece293143504c
Grigoriy Pridatkn <pridatkin@gmail.com>
2024-09-11 11:24:27 +0300

    Add template

--- News/views.py
+++ News/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .models import News
 
 def index(request):
 	news = News.objects.all()
-	res = '<h1>News list</h1>'
-	for i in news:
-		res += f'<div>\n<p>{i.title}</p>\n<p>{i.content}</p>\n</div>\n<hr>\n'
-	return HttpResponse(res)
+	return render(request, 'index.html', {'news': news, 'title': 'News list'})
